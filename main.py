2c5ebb6ff56cfd619c0d7dfd52737585d219fce7
AsiaAlrashed <158853803+AsiaAlrashed@users.noreply.github.com>
2025-02-10 16:38:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-# # import nltk
 
-# # # تحديد مسار التنزيل داخل البيئة الافتراضية
-# # nltk.data.path.append("E:/Task7/mage-env/nltk_data")
-
-# # # تنزيل punkt
-# # nltk.download("punkt", download_dir="E:/Task7/mage-env/nltk_data")
-import nltk
-from nltk.tokenize import sent_tokenize
-
-# تحديد مسار nltk_data يدوياً
-nltk.download("punkt", download_dir="E:/Task7/mage-env/nltk_data")
-
-text = "Hello! This is a test. How are you?"
-print(sent_tokenize(text))
